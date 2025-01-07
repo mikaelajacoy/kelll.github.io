bf2219c65b4c240835626390c6643cd402723520
mikaelajacoy <165372572+mikaelajacoy@users.noreply.github.com>
2025-01-07 18:52:32 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-
+document.querySelectorAll('.btn').forEach(button => {
+  button.addEventListener('click', () => {
+    button.style.transform = 'scale(0.9)';
+    setTimeout(() => {
+      button.style.transform = 'scale(1)';
+    }, 150);
+  });
+});
